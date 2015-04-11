c49327c74d977194e5d5597cade80147ef599a8c
Your Name <you@example.com>
2015-04-11 10:18:34 +0000

    with loader

---
+++ mapper.rb
@@ -0,0 +1,7 @@
+File.readlines('names.txt').each do |line| 
+	name = line.split(" ").first.downcase
+	`curl -XDELETE 'http://localhost:9200/#{name}/'` 
+	`curl -XPUT 'http://localhost:9200/#{name}/'`
+	`curl -XPUT 'http://localhost:9200/#{name}/_mapping/differ' -d '{\"properties\" : {\"place\" : {\"type\":\"long\"},\"diff\" : {\"type\":\"string\"},\"alt\" : {\"type\":\"string\"}}}'`
+	puts name
+end
